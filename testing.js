3feebe48d8bccec9a37b8111c4f320e6bb11568e
sourabhkt1997 <sourabh.dhanarajan@gmail.com>
2022-10-23 19:37:02 +0530

    check prime number or not

---
+++ testing.js
@@ -0,0 +1,15 @@
+//find the prime number from 1 to 20 including 20
+
+for(let i=1;i<=20;i++){
+let count=0
+ for(let j=1;j<=20;j++){
+   if(i%j==0){
+     count++
+  }
+ }
+if(count!=2){
+ console.log(i,"prime number")
+}else{
+console.log(i,"non prime number")
+}
+}
