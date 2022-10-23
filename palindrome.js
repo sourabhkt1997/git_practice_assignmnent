4e23df56ecd490a720eddcaf340724a8de322413
sourabhkt1997 <sourabh.dhanarajan@gmail.com>
2022-10-23 20:32:17 +0530

    check palindrome

---
+++ palindrome.js
@@ -0,0 +1,13 @@
+//Check whether a string is palindrome or not.
+
+
+let str="malayalam"
+let bag=""
+for(let i=str.length-1;i>=0;i--){
+  bag+=str[i];
+}
+if(bag  !=str){
+  console.log("it is a paliandrome");
+}else {
+  console.log("it is not a palindrome")
+}
