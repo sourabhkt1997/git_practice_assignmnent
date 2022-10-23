6cdbb57cc39aff9a9fb1d64d5291d00685255557
RSsharma7877 <112048304+RSsharma7877@users.noreply.github.com>
2022-10-23 21:02:37 +0530

    code correction done

--- palindrome.js
+++ palindrome.js
@@ -6,7 +6,7 @@ let bag=""
 for(let i=str.length-1;i>=0;i--){
   bag+=str[i];
 }
-if(bag  !=str){
+if(bag  ==str){
   console.log("it is a paliandrome");
 }else {
   console.log("it is not a palindrome")
